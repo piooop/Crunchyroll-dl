fef6d509ec7e460d8a6bf25e47b4a3f68e8133ff
PepeElGrasas <49036522+piooop@users.noreply.github.com>
2020-10-13 16:16:23 +0200

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -1,7 +1,6 @@
 IF NOT EXIST .\youtube-dl.exe (
         wget https://youtube-dl.org/downloads/latest/youtube-dl.exe
-        del .\wget.exe
-)        
+)   
 set /p PW=Crunchyroll link: 
 set /p lang=Language? Example. esES, enUS: 
 youtube-dl.exe --sub-lang %lang% --write-sub -o 2.mp4 %PW%
