f0d0f798f9f77611a88902b62d3e5be0de6235fe
PepeElGrasas <49036522+piooop@users.noreply.github.com>
2020-10-13 16:14:41 +0200

    Add files via upload

---
+++ Run.bat
@@ -0,0 +1,19 @@
+IF NOT EXIST .\youtube-dl.exe (
+        wget https://youtube-dl.org/downloads/latest/youtube-dl.exe
+        del .\wget.exe
+)        
+set /p PW=Crunchyroll link: 
+set /p lang=Language? Example. esES, enUS: 
+youtube-dl.exe --sub-lang %lang% --write-sub -o 2.mp4 %PW%
+IF EXIST 2.mp4 (
+     exit
+)
+youtube-dl.exe -o --sub-lang %lang% --write-sub 2.mp4 %PW%
+IF EXIST 2.mp4 (
+     exit
+)
+youtube-dl.exe -o 2.mp4 %PW% --sub-lang %lang% --write-sub
+IF EXIST 2.mp4 (
+     exit
+)
+youtube-dl.exe -o 2.mp4 --sub-lang %lang% --write-sub %PW% 
